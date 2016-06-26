12439b43e3e65c551a3f9024071b6f4ae60fca2b
Logan Wright <logan.william.wright@gmail.com>
2016-06-26 15:03:53 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "SecretSocks",
     dependencies: [
-    	.Package(url: "https://github.com/czechboy0/Socks.git", majorVersion: 0, minor: 7),
+    	.Package(url: "https://github.com/czechboy0/Socks.git", majorVersion: 0, minor: 8),
     	.Package(url: "https://github.com/qutheory/ssl.git", majorVersion: 0, minor: 1),
     ],
     targets: [
