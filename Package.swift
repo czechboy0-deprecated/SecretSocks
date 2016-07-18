c491ed763f9df595d3b520dbf9f63b4d4c577256
Logan Wright <logan.william.wright@gmail.com>
2016-07-18 14:33:14 -0400

    preview 2

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "SecretSocks",
     dependencies: [
     	.Package(url: "https://github.com/czechboy0/Socks.git", majorVersion: 0, minor: 8),
-    	.Package(url: "https://github.com/qutheory/tls.git", majorVersion: 0, minor: 2),
+    	.Package(url: "https://github.com/qutheory/tls.git", majorVersion: 0, minor: 3),
     ],
     targets: [
         Target(name: "SecretSocks")
